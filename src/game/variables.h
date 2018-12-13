5ad30c1e6c4abddd73d252ddf87a1117b1a889a5
sepulzera <hartung_spam@gmx.de>
2018-12-13 17:37:49 +0100

    *Increase max health to 6

--- src/game/variables.h
+++ src/game/variables.h
@@ -128,7 +128,7 @@ MACRO_CONFIG_INT(SvVoteKick, sv_vote_kick, 1, 0, 1, CFGFLAG_SAVE|CFGFLAG_SERVER,
 MACRO_CONFIG_INT(SvVoteKickMin, sv_vote_kick_min, 0, 0, MAX_CLIENTS, CFGFLAG_SAVE|CFGFLAG_SERVER, "Minimum number of players required to start a kick vote")
 MACRO_CONFIG_INT(SvVoteKickBantime, sv_vote_kick_bantime, 5, 0, 1440, CFGFLAG_SAVE|CFGFLAG_SERVER, "The time to ban a player if kicked by vote. 0 makes it just use kick")
 
-MACRO_CONFIG_INT(SvVampireMaxHealth, sv_vampire_health, 5, 1, 10, CFGFLAG_SAVE|CFGFLAG_SERVER, "Health limit")
+MACRO_CONFIG_INT(SvVampireMaxHealth, sv_vampire_health, 6, 1, 10, CFGFLAG_SAVE|CFGFLAG_SERVER, "Health limit")
 MACRO_CONFIG_INT(SvLaserjumps, sv_laserjumps, 1, 0, 1, CFGFLAG_SAVE|CFGFLAG_SERVER, "1 = Laserjumps")
 MACRO_CONFIG_INT(SvKillingSpreeMsg, sv_killingspree_msg, 1, 0, 1, CFGFLAG_SAVE|CFGFLAG_SERVER, "0 = no killing spree announcements")
 
